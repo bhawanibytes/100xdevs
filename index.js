878fc231abae88c8e8f22709f1f323ffc8c7ab89
Bhawani Singh <bs190520@gmail.com>
2024-10-02 03:12:28 +0530

    db's schema done

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express")
 const { userRouter } = require("./routes/user");
-const { courseRouter, adminRouter } = require("./routes/admin");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 const app = express()
+const mongoose = require("mongoose");
 
 app.use(express.json());
-app.use()
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
 
-// app.get("/", (req, res) => {
-    
-// })
+async function main() {
+    require('dotenv').config()
+    await mongoose.connect(`${process.env.dbConnectionString}`);
+    console.log("db is connected")
+    app.listen(`${process.env.PORT}`)
+    console.log(`Listening on PORT: ${process.env.PORT}`);
+}
 
-app.listen(3000)
+main();
