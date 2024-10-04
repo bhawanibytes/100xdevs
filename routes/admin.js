d7da2628e764c4cd6e6d776c5593272b4b872d6e
Bhawani Singh <bs190520@gmail.com>
2024-10-04 18:44:17 +0530

    added auth endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,39 @@
 const { Router } = require("express");
 const { adminModel } = require("../db");
 const adminRouter = Router();
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "ADMINALADIN"
 
-adminRouter.post("/signup", (req, res) => {
+adminRouter.post("/signup", async function (req, res) {
+    const { email, password, firstName, lastName } = req.body
+    await adminModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    })
     res.json({
         message: "signup endpoint"
     })
 })
 
-adminRouter.post("/signin", (req, res) => {
-    res.json({
-        message: "signin endpoint"
+adminRouter.post("/signin", async function (req, res)  {
+    const { email, password, } = req.body
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
+    });
+    if(admin){
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_USER_PASSWORD)
+        // Do cookie logic
+        res.status(200).json({
+            token: token
+        })
+    }
+    res.status(403).json({
+        message: "wrong credentials"
     })
 })
 
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,39 @@
-const { Router } = require("express")
+const { Router } = require("express");
+const { userModel } = require("../db");
 const userRouter = Router();
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "USERALADIN"
 
-userRouter.post("/signup", (req, res) => {
+userRouter.post("/signup", async function(req, res) {
+    const { email, password, firstName, lastName } = req.body
+    await userModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    })
     res.json({
         message: "signup endpoint"
     })
 })
 
-userRouter.post("/signin", (req, res) => {
-    res.json({
-        message: "signin endpoint"
+userRouter.post("/signin", async function (req, res)  {
+    const { email, password, } = req.body
+    const user = await userModel.findOne({
+        email: email,
+        password: password
+    });
+    if(user){
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_USER_PASSWORD)
+        // Do cookie logic
+        res.status(200).json({
+            token: token
+        })
+    }
+    res.status(403).json({
+        message: "wrong credentials"
     })
 })
 
