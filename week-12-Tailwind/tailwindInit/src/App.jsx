26869a14e803bf2e12dbcab38ffe5c32fcdba409
Bhawani Singh <bs190520@gmail.com>
2025-06-10 19:57:21 +0530

    tailwind Initial setup

---
+++ week-12-Tailwind/tailwindInit/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <div className="h-screen bg-slate-700">
+
+      <div className='text-blue-500'>Hello World</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
